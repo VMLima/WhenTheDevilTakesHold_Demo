d248254ca804429f2843dc57289d8fe6429a664e
Silverbird1 <victoria.m.lima2@gmail.com>
2019-03-05 22:21:58 -0800

    First weeks work on final assignment

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -7,66 +7,62 @@ public class Controller : MonoBehaviour {
     public float speed;
     public float mouseSpeed;
     public float jumpSpeed;
-    public float maxSpeed;
-
-    //public Transform leave;
 
     private bool isGrounded = true;
+    private bool crouched;
 
     private Vector3 movement;
-    private Vector3 rotation;
     private Rigidbody rig;
     public Camera cam;
 
-    private float y;
-    private float x;
+    private float InputY;
+    private float InputX;
+    private float MouseY;
+    private float MouseX;
+
 
     void Start() {
         rig = GetComponent<Rigidbody>();
         rig.maxAngularVelocity = mouseSpeed;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     void FixedUpdate() {
 
-        // Handles movement
-        if (Input.GetKey(KeyCode.W)) {
-            movement = transform.forward * speed;
-        } else if (Input.GetKey(KeyCode.A)) {
-            movement = -transform.right * speed;
-        } else if (Input.GetKey(KeyCode.S) ) {
-            movement = -transform.forward * speed;
-        } else if (Input.GetKey(KeyCode.D)) {
-            movement = transform.right * speed;
-        } else {
-            movement = Vector3.zero;
-        }
+        InputX = Input.GetAxisRaw("Horizontal");
+        InputY = Input.GetAxisRaw("Vertical");
 
-        if (rig.velocity.magnitude > maxSpeed) {
-            rig.velocity = rig.velocity.normalized * maxSpeed;
-        }
+        movement = (InputX * transform.right + InputY * transform.forward).normalized * speed;
 
-        movement.y = rig.velocity.y;
-        rig.position += movement * Time.deltaTime;
+        transform.position += movement * Time.deltaTime;
 
         //Handles mouse rotation
-        //if (!camSwapped) {
-        //y -= Input.GetAxis("Mouse Y") * mouseSpeed;
-        x += Input.GetAxis("Mouse X") * mouseSpeed;
-        //cam.transform.position = Quaternion.AngleAxis(x, Vector3.up);
-        transform.eulerAngles = new Vector3(0, x, 0);
-        //offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * mouseSpeed, Vector3.up) * offset;
-        //Vector3 normOffset = Vector3.Normalize(offset);
-        //normOffset.y = 4;
-        //normOffset.x *= 4;
-        //normOffset.z *= 4;
-        //cam.transform.position = transform.position + normOffset;
-        //cam.transform.LookAt(transform.position);
-        //}
+        MouseX += Input.GetAxis("Mouse X") * mouseSpeed;
+        MouseY += Input.GetAxis("Mouse Y") * mouseSpeed;
+
+        transform.eulerAngles = new Vector3(0, MouseX, 0);
+        cam.transform.eulerAngles = new Vector3(Mathf.Clamp(-MouseY, -90, 90), MouseX, 0);
 
         // Handles jumping
         if (Input.GetKey(KeyCode.Space) && isGrounded) {
             rig.velocity = transform.up * jumpSpeed;
             isGrounded = false;
+        }
+
+        // Crouching 
+        if (Input.GetKeyDown(KeyCode.LeftControl)) {
+            cam.transform.localPosition = new Vector3(0, 0, 0);
+            crouched = true;
+        } else if (Input.GetKeyUp(KeyCode.LeftControl)){
+            cam.transform.localPosition = new Vector3(0, 0.75f, 0);
+            crouched = false;
+        }
+
+        // Reveals cursor
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Cursor.visible = !Cursor.visible;
+            Cursor.lockState = CursorLockMode.None;
         }
 
     }
@@ -75,11 +71,5 @@ public class Controller : MonoBehaviour {
         if (collision.gameObject.CompareTag("Ground")) {
             isGrounded = true;
         }
-    }
-
-    private void OnTriggerEnter(Collider other) {
-        //if (other.CompareTag("Exit")) {
-        //    leave.gameObject.SetActive(true);
-        //}
     }
 }
---
+++ Assets/Scripts/InteractableScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableScript : MonoBehaviour {
+
+    public GameObject player;
+    private bool interacting;
+    private ParticleSystem[] particles;
+
+    void Start() {
+        particles = transform.GetComponentsInChildren<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.E)) interacting = true;
+        else interacting = false;
+    }
+
+    public void ActivateShrine() {
+        if (interacting) {
+            for (int i = 0; i < particles.Length; i++) {
+                particles[i].Play();
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (other.CompareTag("Player")) {
+            ActivateShrine();
+        }
+    }
+}
